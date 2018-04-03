fb6d663c23d3eeef4f5afce989811e7b3a87c532
Thipavong Ryan <150052234@cart.ads>
2018-04-03 15:28:19 -0700

    made progress on gallery

--- yes.js
+++ yes.js
@@ -10,22 +10,8 @@ function curPic(n){
 }
 
 function showPic(n){
-    var i;
     var photo = document.getElementsByClassName("gal");
     var view = document.getElementsByClassName("view");
     
-    if (n > photo.length) {picInd = 1}
     
-    if (n < 1) {picInd = photo.length}
-    
-    for (i = 0; i < photo.length; i++){
-        photo[i].style.display = "none";
-    }
-    
-    for (i = 0; i < view.length; i++) {
-        view[i].className = view[i].className.replace(" active", "");
-    }
-    
-    photo[picInd-1].style.display = "block";
-    view[picInd-1].className += " active";
 }
