4580b4dcc39dde2f319765dfcb65623e578abdbc
Thipavong Ryan <150052234@cart.ads>
2018-04-04 15:23:57 -0700

    new things and media queiries!

---
+++ slideshow.js
@@ -0,0 +1,18 @@
+var picInd = 0;
+showPic();
+
+function showPic(){
+    var slide = document.getElementsByClassName("gal");
+    
+    for (var i = 0; i < slide.length;i++) {
+        slide[i].style.display = "none";
+    }
+    picInd++
+    
+    if (picInd > slide.length) {
+        picInd = 1;
+    }
+    
+    slide[picInd-1].style.display = "block";
+    setTimeout(showPic, 5000);
+}
--- yes.js
+++ yes.js
@@ -1,17 +1,9 @@
-var picInd = 1;
-showPic(picInd);
-
-function conPic(n){
-    showPic(picInd += n);
-}
-
-function curPic(n){
-    showPic(picInd = n);
-}
-
-function showPic(n){
-    var photo = document.getElementsByClassName("gal");
-    var view = document.getElementsByClassName("view");
+function showPic() {
+    var thing = document.getElementById("haha").children;
+    var len = thing.length;
     
-    
+    if (len == 1) {
+        thing.addClass = "bigger";
+        console.log("here");
+    }
 }
