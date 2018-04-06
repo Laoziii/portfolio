f1b491b2fc0c436295247526ce72b4a231c05496
Thipavong Ryan <150052234@cart.ads>
2018-04-06 15:19:32 -0700

    did things that are cool

--- yes.js
+++ yes.js
@@ -1,9 +1,29 @@
-function showPic() {
-    var thing = document.getElementById("haha").children;
-    var len = thing.length;
-    
-    if (len == 1) {
-        thing.addClass = "bigger";
-        console.log("here");
-    }
+var thinga = document.getElementById("heha");
+
+var thingb = document.getElementById("hehaa");
+
+var thingc = document.getElementById("hehaaa");
+
+function showLor() {
+    thinga.style.display = "block";
+}
+
+function closeLor() {
+    thinga.style.display = "none";
+}
+
+function showLorw() {
+    thingb.style.display = "block";
+}
+
+function closeLorw() {
+    thingb.style.display = "none";
+}
+
+function showLorm() {
+    thingc.style.display = "block";
+}
+
+function closeLorm() {
+    thingc.style.display = "none";
 }
